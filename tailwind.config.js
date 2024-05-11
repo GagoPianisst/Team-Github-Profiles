2dda2d50d8f26059ec5cbaba743d06a6558d6805
beyerperez123@gmail.com <beyerperez123@gmail.com>
2024-05-10 23:02:33 -0600

    ⭐Avance 2 Instalacion de daisyUI y trabajo en App.vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwincss').Config} */
 export default {
   content: [
@@ -7,6 +9,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
 
