05e0da036e831bd8878cac891e0e71945e5cf119
beyerperez123@gmail.com <beyerperez123@gmail.com>
2024-05-10 19:54:05 -0600

    ⭐Avance 1 Instalacion y configuracion de TailWind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwincss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
